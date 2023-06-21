8063ede3cfca87bb915e9d10ede3ef15b1c84c31
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-21 14:11:10 +0700

    add: constant object

---
+++ app/src/main/java/com/exfarnanda1945/composemahasiswa/utils/Constant.kt
@@ -0,0 +1,11 @@
+package com.exfarnanda1945.composemahasiswa.utils
+
+object Constant {
+    const val DATABASE_NAME ="mahasiswa"
+
+    const val STUDENT_TABLE_NAME = "mahasiswa"
+    const val LECTURER_TABLE_NAME = "dosen"
+    const val COURSE_TABLE_NAME = "matakuliah"
+
+
+}
