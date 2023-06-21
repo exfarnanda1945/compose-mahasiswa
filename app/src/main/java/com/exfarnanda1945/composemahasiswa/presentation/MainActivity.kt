2b13abce11f8a5b9ad07c0f21189840f9dfa4919
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-21 14:22:32 +0700

    add: setup navigation at main activity

--- app/src/main/java/com/exfarnanda1945/composemahasiswa/presentation/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/composemahasiswa/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.exfarnanda1945.composemahasiswa
+package com.exfarnanda1945.composemahasiswa.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.exfarnanda1945.composemahasiswa.ui.theme.ComposeMahasiswaTheme
+import com.exfarnanda1945.composemahasiswa.utils.navigation.SetupNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navHostController = rememberNavController()
+                    SetupNavGraph(navHostController = navHostController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeMahasiswaTheme {
-        Greeting("Android")
     }
 }
